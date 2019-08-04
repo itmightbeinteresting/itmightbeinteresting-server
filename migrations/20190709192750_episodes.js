19b79d48941dfe68dd21091c1fe3a27d24b68634
Josh Levy <joshaaronlevy@gmail.com>
2019-08-03 22:14:06 -0600

    Updated episode routes

--- migrations/20190709192750_episodes.js
+++ migrations/20190709192750_episodes.js
@@ -5,7 +5,6 @@ exports.up = function (knex, Promise) {
     episodes.string('title');
     episodes.string('slug');
     episodes.string('website_url');
-    episodes.string('download_url');
     episodes.string('embed_url');
     episodes.string('itunes_url');
     episodes.string('youtube_url');
